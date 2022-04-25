a4937ec83268b16411f64e2c21d583a913284adb
Your Name <you@example.com>
2022-04-25 13:42:01 -0400

    Final Commit

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,10 +2,11 @@
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p> No Feedback Yet</p>;
   }
   //Add and delete with animation
@@ -29,7 +30,9 @@ function FeedbackList() {
   //     </AnimatePresence>
   //   </div>
   // );
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       {feedback.map((item) => (
         <FeedbackItem key={item.id} item={item} />
